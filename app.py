2b69c7a338f02b2a1c6dca91c6c4e8eed33ab01a
DESKTOP-PA4PET0\User <it18041880@my.sliit.lk>
2021-06-05 19:58:00 +0530

    flaskFloral

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask
-import engine
+#import engine
 app = Flask(__name__)
 
-@app.route("/api/v1.0/recommendations/<int:id>", methods=["GET"])
-def get_recomendations(id):
-    print("product_id: " + str(id))
-    return engine.get_recommendations(id)
+@app.route("/api/v1.0/recommendations/", methods=["GET"])
+def get_recomendations():
+    print("product_id: " + str(3))
+    return 3
+  #  return engine.get_recommendations(id)
 
 if __name__ == "__main__":
     app.run(host= '0.0.0.0')
