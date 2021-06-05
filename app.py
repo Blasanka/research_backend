4c08558013d59e3576c34c84d6ddd84d046edbeb
Blasanka <blasanka95@gmail.com>
2021-06-05 20:20:44 +0530

    Internal server error fixed

--- app.py
+++ app.py
@@ -2,13 +2,12 @@ from flask import Flask
 #import engine
 app = Flask(__name__)
 
-@app.route("/api/v1.0/recommendations/", methods=["GET"])
-def get_recomendations():
-    print("product_id: " + str(3))
-    return 3
+@app.route("/api/v1.0/recommendations/<int:id>", methods=["GET"])
+def get_recomendations(id):
+    print("product_id: " + str(id))
+    return str(id)
   #  return engine.get_recommendations(id)
 
 if __name__ == "__main__":
     app.run(host= '0.0.0.0')
-    
-    
+
