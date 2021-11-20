863e1680e9d6f7fb7880635b95343dd6187b9a8b
IT15148414 <it15148414@my.sliit.lk>
2021-11-20 18:55:00 +0530

    Improved model embeded and class prediction conditions improved

--- disease/util.py
+++ disease/util.py
@@ -13,34 +13,41 @@ __model = None
 
 
 def detect_disease(file, upload_folder):
+    
+    if not os.path.exists(upload_folder):
+        os.makedirs(upload_folder)
+    
     # Saving upload file for future use
     f_name = str(uuid.uuid4()) + '_' + file.filename
     file.save(os.path.join(upload_folder, f_name))
 
-    cnn_model = ker.models.load_model("disease/artifacts/flower_disease_cnn.h5")
+    cnn_model = ker.models.load_model("disease/artifacts/flower_disease_identifier.h5")
     cnn_model.summary()
 
-    if not os.path.exists('uploads'):
-        os.makedirs('uploads')
-    
-    predict_image = image.load_img('uploads\\'+f_name, target_size=(64, 64, 1))
+    # predict_image = image.load_img('uploads\\'+f_name, target_size=(64, 64, 1))
+    # predict_image = image.img_to_array(predict_image)
+    # predict_image = np.expand_dims(predict_image, axis=-1)
+    # predict_image = predict_image.reshape(-1, 64, 64, 1)
+    # predicted_result = cnn_model.predict(predict_image)
+    # predicted_class = cnn_model.predict_classes(predict_image)
+
+    predict_image = image.load_img('uploads\\'+f_name, target_size=(64, 64))
     predict_image = image.img_to_array(predict_image)
-    predict_image = np.expand_dims(predict_image, axis=-1)
-    predict_image = predict_image.reshape(-1, 64, 64, 1)
+    predict_image = np.expand_dims(predict_image, axis=-0)
     predicted_result = cnn_model.predict(predict_image)
-    predicted_class = cnn_model.predict_classes(predict_image)
-    
+    predicted_class = np.argmax(predicted_result, axis=-1)
+
     print(predicted_class)
     print(predicted_result)
     
     output = predicted_result[0]
 
-    if predicted_result[0][0] == 0:
+    if predicted_class[0] == 0:
         disease_name = "Rose healthy"
         disease_description = "Plant and flower is healthy"
         flower_name = "Rose"
         prediction = output[0]
-    elif predicted_result[0][0] == 1:
+    elif predicted_class[0] == 1:
         disease_name = "Powdery Mildow"
         disease_description = "Powdery Mildow is a fungas disease creating devastating losses. " \
                               "Powdery Mildow thrives in cool, humid weather."
@@ -51,26 +58,26 @@ def detect_disease(file, upload_folder):
         disease_description = "The fungus can invade and damage almost any plant part."
         flower_name = "Rose"
         prediction = output[2]
-    elif predicted_class[0] == 2:
+    elif predicted_class[0] == 3:
         disease_name = "Sunflower healty"
         disease_description = "Plant and flower is healthy"
         flower_name = "Sunflower"
         prediction = output[3]
-    elif predicted_class[0] == 3:
+    elif predicted_class[0] == 4:
         disease_name = "Wilt"
         disease_description = "Tissue between leaf veins become yellow, then brown, giving diseased leaves a mottled " \
                               "appearance. "
         flower_name = "Sunflower"
         prediction = output[4]
-    elif predicted_class[0] == 1:
+    elif predicted_class[0] == 5:
         disease_name = "Root Rot"
         disease_description = "The infected plants show drooping of leaves and death occurs in patches."
         flower_name = "Sunflower"
         prediction = output[5]
     else:
-        disease_name = "Sunflower healthy"
-        disease_description = "Plant and flower is healthy"
-        flower_name = "Sunflower"
+        disease_name = "Could not identify"
+        disease_description = "Please try again"
+        flower_name = "No name"
         prediction = output[3]
 
     return {
