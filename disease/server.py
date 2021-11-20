e747c1cf902bbf2573ebb6c87a9650fd836df91f
IT15148414 <it15148414@my.sliit.lk>
2021-11-20 19:47:09 +0530

    Sub dir server script removed

--- disease/server.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, request, jsonify
-import util
-
-app = Flask(__name__)
-
-
-if __name__ == "__main__":
-    print("Starting Python Flask Server For Home Price Prediction")
-    app.run()
