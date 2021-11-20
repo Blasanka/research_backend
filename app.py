673469d095a7f96e210dcfe65c279572926e6343
IT15148414 <it15148414@my.sliit.lk>
2021-11-20 16:19:08 +0530

    Base URL changed to prod

--- app.py
+++ app.py
@@ -35,5 +35,5 @@ def transfer():
 
 
 if __name__ == "__main__":
-    app.run(host="https://flower-disease-cnn.herokuapp.com", use_reloader=False)
+    app.run(host="flower-disease-cnn.herokuapp.com", use_reloader=False)
 
